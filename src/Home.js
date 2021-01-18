c8890d8f31378918e9b33dcbff9d870f74cc6ee1
Omnivoro <omnivoro@gmail.com>
2021-01-18 18:19:58 +0000

    Event parameter passed to the function.
    The event handler has a parameter that allows to get information about
    the event.

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,15 @@ const Home = () => {
   const handleClick = () =>{
 	  console.log("Hello, ninjas");
   }
-  const handleClickAgain = (name) =>{
-	  console.log("Hello " +  name);
+  const handleClickAgain = (name, event) =>{
+	  console.log("Hello " +  name, e.target);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
 	  <button onClick={handleClick}>Click me</button>
-	  <button onClick={()=>{handleClickAgain('mario')}}>Click me again</button>
+	  <button onClick={(e)=>{handleClickAgain('mario', e)}}>Click me again</button>
     </div>
   );
 }
@@ -20,3 +20,8 @@ export default Home;
 // v07t01:44 The handleClick function is not writen as handleClick() because
 // the parenthesis would invoke the funcion when the page was uploaded
 // in the browser
+
+// v07t03:28 The handleClickAgain funtion must be wrapped by an anonimous function in order to pass the value of the name variable. As this anonimous function has only one instruction, the curly brackets that wraps handleClickAgain('mario') could be omitted.
+
+// v07t05:40 The event parameter "e" can be use to get aditional information 
+// about the event.
