583ccf79095c6274f3636119d316328c07fc26e3
Omnivoro <omnivoro@gmail.com>
2021-01-18 18:11:18 +0000

    A second button was crated.
    The onClick event calls an anonimous function that wraps the function
    HandleClickAgain. This last function shows a message in the console.

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,21 @@ const Home = () => {
   const handleClick = () =>{
 	  console.log("Hello, ninjas");
   }
+  const handleClickAgain = (name) =>{
+	  console.log("Hello " +  name);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
 	  <button onClick={handleClick}>Click me</button>
+	  <button onClick={()=>{handleClickAgain('mario')}}>Click me again</button>
     </div>
   );
 }
  
 export default Home;
 
-// The handleClick function is not writen as handleClick() because
+// v07t01:44 The handleClick function is not writen as handleClick() because
 // the parenthesis would invoke the funcion when the page was uploaded
 // in the browser
