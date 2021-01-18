8d29c307d21bcf932c9ef9c642ea16747b1ad9e5
Omnivoro <omnivoro@gmail.com>
2021-01-18 17:57:56 +0000

    Button created in Home component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 const Home = () => {
+  const handleClick = () =>{
+	  console.log("Hello, ninjas");
+  }
   return (
     <div className="home">
       <h2>Homepage</h2>
+	  <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
  
 export default Home;
+
+// The handleClick function is not writen as handleClick() because
+// the parenthesis would invoke the funcion when the page was uploaded
+// in the browser
