1ebcbc30102d2796ae1e7425f6e400ad46778e33
Omnivoro <omnivoro@gmail.com>
2021-01-18 15:52:47 +0000

    Lesson 04 - Dynamic values in Template
    We studied which type of values are accepted by return.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "http://www.google.com"
+  //Neither  objects nor boolean values can be passed to return.
+  //const person = {name: 'Yoshi', age:30};
+  //const boolean = false;
   return (
     <div className="App">
       <div className="content">
-		  <h1>App Component</h1>
+		  <h1>{title}</h1>
+		  <p>Likes {likes} times</p>
+		  {/*<p>{person}</p>*/}
+		  <p>{10}</p>
+		  <p>{"A string value"}</p>
+		  <p>{Math.random() * 10}</p>
+		 <a href={link}>Google Site</a>
 	  </div>
     </div>
   );
