249bd35e1c65517f6fa226c8953e42ab6a2a76c6
Omnivoro <omnivoro@gmail.com>
2021-01-20 19:15:09 +0000

    Adding a dependency to the useEffect() function
    Any element of the array used as the second parameter is a dependency
    whose value will be render, when its set function be invoked.

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  
+  const [name, setName] = useState('mario');
 	
   const handleDelete = (id) =>{
 	  const newBlogs = blogs.filter((blog)=>blog.id !== id);
@@ -16,19 +18,23 @@ const Home = () => {
   
   useEffect(()=>{
 	  console.log('use effect ran!');
-	  console.log(blogs);
-  }, []);
+	  console.log(name);
+  }, [name]);
 	
   /* --	The useEffect() function might have a second parameter. This parameter is
 	   	an array, and each one of its element is a dependency. 
      --	When the useEffect() function doesn't have this second parameter, 
   		it always renders all variables of the page.
 	 -- When the useEffect() function has no dependencies, so the second parameter is an empty array, 
-  		then it just renders variables once at the first time.*/
+  		then it just renders variables once at the first time.
+	 -- Only the dependencies, in the other words, the array elements of the second parameter,
+	    will be render when the setVariable() function associeted to those dependencies be invoked.*/
     
   return (
     <div className="home">
     	<Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+		<button onClick={()=>setName('luigi')}>Change name</button>
+		<p>{name}</p>
 	</div>
   );
 }
