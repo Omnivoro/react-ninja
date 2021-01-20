0d1b7595e574a444785fa4d2fa536a0b488bd641
Omnivoro <omnivoro@gmail.com>
2021-01-20 17:37:17 +0000

    Deleting blogs using event handler throught props
    An event onclick handler sends the blog id that will be deleted.
    
    Using a filter function, the handleDelete() funciton saves all the
    blogs except that one that has the id blog received, in a variable
    called 'newBlogs'.
    
    The setBlog() function uses the newBlogs variable to update the blogs
    object without destroying the original one.

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,6 +1,5 @@
-const Bloglist = ({blogs, title}) =>{
-	//const blogs = props.blogs;
-	//const title = props.title;
+const Bloglist = ({blogs, title, handleDelete}) =>{
+	
 	
 	return(
 	<div className="blog-list">
@@ -9,6 +8,7 @@ const Bloglist = ({blogs, title}) =>{
 			<div className="blog-preview" key={blog.id}>
 			<h2>{blog.title}</h2>
 			<p>Written by {blog.author}</p>
+			<button onClick={()=>handleDelete(blog.id)}>Delete blog</button>
 		</div>))}
 	</div>);
 }
--- src/Home.js
+++ src/Home.js
@@ -8,11 +8,14 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
-
+	
+  const handleDelete = (id) =>{
+	  const newBlogs = blogs.filter((blog)=>blog.id !== id);
+	  setBlogs(newBlogs);
+  }
   return (
     <div className="home">
-    	<Bloglist blogs={blogs} title="All Blogs!"/>
-		<Bloglist blogs={blogs.filter((blogs)=>blogs.author==='mario')} title="Mario's Blogs!"/>
+    	<Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
 	</div>
   );
 }
