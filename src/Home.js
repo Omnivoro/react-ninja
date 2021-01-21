5b34e21c18686f35e017cc65bfc8255540e9dcae
Omnivoro <omnivoro@gmail.com>
2021-01-21 19:07:28 +0000

    Testing axios request and wrting the posts json
    Using the https://jsonplaceholder.typicode.com/posts url to test axios
    and showing data in the console.

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,24 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import axios from 'axios';
+
 
 const Home = () => {
 	
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    {
+    "userId": 1,
+    "id": 10,
+    "title": "optio molestias id quia eum",
+    "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+  },
+  {
+    "userId": 2,
+    "id": 11,
+    "title": "et ea vero quia laudantium autem",
+    "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+  }
+  ]);
 
   const handleDelete = (id) =>{
 	  const newBlogs = blogs.filter((blog)=>blog.id !== id);
@@ -15,9 +26,11 @@ const Home = () => {
   }
   
   useEffect(()=>{
-	  console.log('use effect ran!'); 
-  });
-	
+	  axios.get('https://jsonplaceholder.typicode.com/posts')
+		  .then(res => console.log(res))
+		  .catch(err => console.log(err));
+  },[]);
+
   return (
     <div className="home">
     	<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
