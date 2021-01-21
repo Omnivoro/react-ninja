4e3ddeef4ee5b667ae79dd4b43c79a5b348d8267
Omnivoro <omnivoro@gmail.com>
2021-01-21 20:08:11 +0000

    Getting ready the Home component to the next step

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,7 @@ import axios from 'axios';
 
 const Home = () => {
 	
-  const [blogs, setBlogs] = useState([
-    {
-    "userId": 1,
-    "id": 10,
-    "title": "optio molestias id quia eum",
-    "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
-  },
-  {
-    "userId": 2,
-    "id": 11,
-    "title": "et ea vero quia laudantium autem",
-    "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
-  }
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
   const handleDelete = (id) =>{
 	  const newBlogs = blogs.filter((blog)=>blog.id !== id);
@@ -27,7 +14,7 @@ const Home = () => {
   
   useEffect(()=>{
 	  axios.get('https://jsonplaceholder.typicode.com/posts')
-		  .then(res => console.log(res))
+		  .then(res => setBlogs(res.data))
 		  .catch(err => console.log(err));
   },[]);
 
