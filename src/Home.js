2c027a63bc96f2b50d6159f6b3e141d2fa21b0ac
Omnivoro <omnivoro@gmail.com>
2021-02-06 16:49:39 +0000

    Making a custom hock - the useFetch hock

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,10 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);	
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/post###')
-      .then(res => {
-		if(!res.ok){
-			throw Error("Could not fetch the data for that resource!");  
-		}
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-		setError(null);
-      }).catch(err => {
-		  setIsPending(false);
-		  setError(err.message)});
-    }, 1000);
-  }, [])
-
+	
+   const {data: blogs, isPending, error} = useFetch('https://jsonplaceholder.typicode.com/posts');	
+	
   return (
     <div className="home">
 	  { error && <div>{error}</div>}
