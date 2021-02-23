99f32a19ead0533f40fd11aeddd1288094db66f1
Omnivoro <omnivoro@gmail.com>
2021-02-23 12:47:37 +0000

    fix: Add the body ant the userId state to the html form

--- src/Create.js
+++ src/Create.js
@@ -3,6 +3,8 @@ import {useState} from 'react';
 const Create = () =>{
 	
 	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [userId, setUserId] = useState('1');
 	
 	return(
 		<div className="create">
@@ -16,15 +18,23 @@ const Create = () =>{
 					onChange={(e)=>setTitle(e.target.value)}
 				/>
 				<label>Blog body:</label>
-				<textarea required></textarea>
+				<textarea 	
+					required
+					value={body}
+					onChange={(e)=>setBody(e.target.value)}
+				></textarea>
 				<label>Blog author:</label>
-				<select>
+				<select 
+					value={userId}
+					onChange={(e)=>setUserId(e.target.value)}>
 					<option value="1">User id - 1</option>
 					<option value="7">User id - 7</option>
 				</select>
 				<button>Add Blog</button>
 			</form>
 			<p>{title}</p>
+			<p>{body}</p>
+			<p>{userId}</p>
 		</div>
 	)
 };
