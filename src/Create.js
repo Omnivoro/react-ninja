985a2d94c6e563bfcd3798af89f94a71677d126d
Omnivoro <omnivoro@gmail.com>
2021-02-23 12:40:04 +0000

    feat: Create a html form and track the title state

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,30 @@
+import {useState} from 'react';
+
 const Create = () =>{
+	
+	const [title, setTitle] = useState('');
 	
 	return(
 		<div className="create">
 			<h2>Add a new blog!</h2>
+			<form>
+				<label>Blog title:</label>
+				<input 
+					type = "text"
+					required
+					value={title}
+					onChange={(e)=>setTitle(e.target.value)}
+				/>
+				<label>Blog body:</label>
+				<textarea required></textarea>
+				<label>Blog author:</label>
+				<select>
+					<option value="1">User id - 1</option>
+					<option value="7">User id - 7</option>
+				</select>
+				<button>Add Blog</button>
+			</form>
+			<p>{title}</p>
 		</div>
 	)
 };
